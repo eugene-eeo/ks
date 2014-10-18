c6029e41ddfa75f04d468445a8470bab9d01c93a
Eugene Eeo <xe0jun@hotmail.com>
2014-10-18 17:36:27 +0800

    better performance for cache-hits in ks.Mapping

--- ks/mapping.py
+++ ks/mapping.py
@@ -28,21 +28,32 @@ class Mapping(_Map):
         self.iterable = iter(iterable)
         self.cache = {}
 
+    def iload(self):
+        """
+        Iteratively load the key-value pairs of the
+        iterable the mapping was instantiated with.
+        """
+        for k, v in self.iterable:
+            self.cache[k] = v
+            yield k, v
+
     def __getitem__(self, key):
         if key in self.cache:
             return self.cache[key]
-        for k, v in self.iterable:
-            self.cache[k] = v
+        for k, v in self.iload():
             if key == k:
                 return v
         raise KeyError
 
     def __iter__(self):
-        for item in self.cache:
-            yield item
-        for k, v in self.iterable:
-            self.cache[k] = v
+        for k in self.cache:
             yield k
+        for k, v in self.iload():
+            yield k
+
+    def __contains__(self, key):
+        return (key in self.cache or
+                key in (k[0] for k in self.iload()))
 
     def __len__(self):
         """
