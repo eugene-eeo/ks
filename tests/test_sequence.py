67d067fe499ba2ec78d34083640a4bfe9835d62b
Eugene Eeo <packwolf58@gmail.com>
2015-08-05 17:44:53 +0800

    Test that IndexError is raised when appropriate

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -8,14 +8,16 @@ class SequenceTest(TestCase):
 
     def test_getitem(self):
         assert self.seq[0] == 1
-        self.seq[2]
         assert self.seq[2] == 3
 
-    def test_len(self):
+    def test_getitem_raises_indexerror(self):
+        self.assertRaises(IndexError, lambda: self.seq[3])
+
+    def test_len_returns_current_size(self):
         assert len(self.seq) == 0
         self.seq[2]
         assert len(self.seq) == 3
 
-    def test_iter(self):
+    def test_iter_preserves_elems(self):
         for _ in range(2):
             assert list(self.seq) == [1, 2, 3]
